e0630bbe88aceb81e5f0295908fe6df647586cf3
ccx1010sky <ccx1010@gmail.com>
2024-04-14 18:12:45 +0100

    zip node/node_modules for production and src.zip

--- .infrastructure/lib/cdk2-stack.ts
+++ .infrastructure/lib/cdk2-stack.ts
@@ -4,7 +4,7 @@ import {
   Cognito, BuildsBucket, QueueFunction, WebRoutes, ZipFunction, githubActions,
 } from '@scloud/cdk-patterns';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { Function } from 'aws-cdk-lib/aws-lambda';
+import { Function, Code} from 'aws-cdk-lib/aws-lambda';
 import { HostedZone, IHostedZone } from 'aws-cdk-lib/aws-route53';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
@@ -155,7 +155,7 @@ export default class Cdk2Stack extends cdk.Stack {
       },
       functionProps: {
         memorySize: 3008,
-        // code: Code.fromBucket(builds, 'api.zip'), // This can be uncommented once you've run a build of the API code
+        code: Code.fromBucket(builds, 'api.zip'), // This can be uncommented once you've run a build of the API code
       },
     });
 
