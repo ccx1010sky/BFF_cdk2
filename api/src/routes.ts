84dd805f53445168adbb4c8e2d7789ebb787fb69
ccx1010sky <ccx1010@gmail.com>
2024-04-15 23:20:48 +0100

    add get_item funciton

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,10 +2,12 @@
 import { Routes } from '@scloud/lambda-api/dist/types';
 import { ping } from './routes/ping';
 import { test } from './routes/test';
+import { getitem } from './routes/getitem';
 
 const routes: Routes = {
   '/api/ping': { GET: ping },
   '/api/ping_cchen': {GET: test},
+  '/api/get_item': {GET: getitem},
 };
 
 export default routes;
---
+++ api/src/routes/getitem.ts
@@ -0,0 +1,45 @@
+/* eslint-disable import/prefer-default-export */
+import { Request, Response } from '@scloud/lambda-api/dist/types';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'; // cc
+import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';// cc
+// Initialize DynamoDB Document Client
+const ddbDocClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
+export async function getitem(request: Request): Promise<Response> {
+  console.log('getitem', request.query);
+
+  // Retrieve item from DynamoDB
+  try {
+    // const { itemId } = request.query; // Assuming itemId is present in the request query
+
+    // Specify the parameters for the GetCommand
+    const params = {
+      TableName: 'cc1Table', // Name of your DynamoDB table
+      Key: {
+        itemId: '1', // Specify the partition key
+        date: '01/01', // Specify the sort key
+      },
+    };
+
+    // Send the GetCommand to DynamoDB and await the result
+    const { Item } = await ddbDocClient.send(new GetCommand(params));
+
+    // Return response based on whether the item was found or not
+    if (Item) {
+      return {
+        statusCode: 200,
+        body: { message: 'Item retrieved successfully', item: Item },
+      };
+    }
+    return {
+      statusCode: 404,
+      body: { message: 'Item not found' },
+    };
+  } catch (error) {
+    console.error('Error retrieving item:', error);
+    return {
+      statusCode: 500,
+      body: { message: 'Internal server error' },
+    };
+  }
+}
