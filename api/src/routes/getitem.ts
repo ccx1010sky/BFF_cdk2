088119097fbf7c7269bb5f1148e5264d52c941cc
ccx1010sky <ccx1010@gmail.com>
2024-04-16 17:24:14 +0100

    modify getitem.ts using DynamoDBClient and GetItemCommand from '@aws-sdk/client-dynamodb'

--- api/src/routes/getitem.ts
+++ api/src/routes/getitem.ts
@@ -1,29 +1,29 @@
 /* eslint-disable import/prefer-default-export */
 import { Request, Response } from '@scloud/lambda-api/dist/types';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'; // cc
-import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';// cc
-// Initialize DynamoDB Document Client
-const ddbDocClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+import { DynamoDBClient , GetItemCommand} from '@aws-sdk/client-dynamodb'; // cc
+// import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';// cc
+
+// Create a DynamoDB client
+const ddbClient = new DynamoDBClient({});
+
 
 export async function getitem(request: Request): Promise<Response> {
   console.log('getitem', request.query);
 
-  // Retrieve item from DynamoDB
-  try {
-    // const { itemId } = request.query; // Assuming itemId is present in the request query
-
-    // Specify the parameters for the GetCommand
+    //Define parameters for the GetItemCommand
     const params = {
       TableName: 'cc1Table', // Name of your DynamoDB table
-      Key: {
-        itemId: '1', // Specify the partition key
-        date: '01/01', // Specify the sort key
+      Key: {itemId: {S:'1'}, // Specify the partition key
+        date: {S:'01/01'}, // Specify the sort key
       },
     };
 
-    // Send the GetCommand to DynamoDB and await the result
-    const { Item } = await ddbDocClient.send(new GetCommand(params));
 
+    // Create a GetItemCommand with the defined parameters
+    const command = new GetItemCommand(params);
+    try {
+    // Send the GetCommand to DynamoDB and await the result
+    const { Item } = await ddbClient.send(command);
     // Return response based on whether the item was found or not
     if (Item) {
       return {
